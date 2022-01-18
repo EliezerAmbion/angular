388e27406e397bfaa7f01c9903d8e85d3c9b340c
EliezerAmbion <eliezerambion@gmail.com>
2022-01-18 12:35:40 +0800

    add typescript

---
+++ typescript/type-assertion.ts
@@ -0,0 +1,6 @@
+// type assertion is to assert what type should you use.
+
+// in here, if you press the .(dot), typescript intellisense will suggest all the things you can do with a string.
+let message: string = 'abc';
+let endsWithC = message.endsWith('c');
+console.log(endsWithC);
