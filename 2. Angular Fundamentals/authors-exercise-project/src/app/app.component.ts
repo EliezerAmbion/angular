0e0428a8dc81b099bf6d9a075b6846019d5747be
EliezerAmbion <eliezerambion@gmail.com>
2022-01-30 13:50:31 +0800

    add input properties docs and sample code

--- 2. Angular Fundamentals/authors-exercise-project/src/app/app.component.ts
+++ 2. Angular Fundamentals/authors-exercise-project/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'authors-exercise-project';
+
+  // let us imagine that the post here is coming from the database. This is just a sample data that is why it is hardcoded.
+  post = {
+    title: 'Test Title',
+    isFavorite: false,
+  };
 }
--- 2. Angular Fundamentals/authors-exercise-project/src/app/favorite/favorite.component.ts
+++ 2. Angular Fundamentals/authors-exercise-project/src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./favorite.component.css'],
 })
 export class FavoriteComponent {
-  isFavorite: boolean = false;
+  @Input() isFavorite: boolean = false;
 
   constructor() {}
 
