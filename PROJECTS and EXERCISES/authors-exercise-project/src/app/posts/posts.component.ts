7bc7842d0f3a48d65fb3011f15bc9b2cc88575f8
EliezerAmbion <eliezerambion@gmail.com>
2022-03-17 17:45:49 +0800

    subscribe only takes one argument

--- PROJECTS and EXERCISES/authors-exercise-project/src/app/posts/posts.component.ts
+++ PROJECTS and EXERCISES/authors-exercise-project/src/app/posts/posts.component.ts
@@ -12,60 +12,31 @@ export class PostsComponent implements OnInit {
   constructor(private postService: PostService) {}
 
   ngOnInit() {
-    this.postService.getPosts().subscribe(
-      (response) => {
-        this.posts = response;
-      },
-      (error) => {
-        console.log(`Unexpected Error has occured: ${error}`);
-      }
-    );
+    this.postService.getPosts().subscribe((response) => {
+      this.posts = response;
+    });
   }
 
   createPost(input: HTMLInputElement) {
     let post: any = { title: input.value };
     input.value = '';
 
-    this.postService.createPost(post).subscribe(
-      (response: any) => {
-        post.id = response.id;
-        this.posts.unshift(post);
-      },
-      (error: Response) => {
-        if (error.status === 400) {
-          // this.form.setError(error.json());
-          console.log();
-        } else {
-          console.log(`Unexpected Error has occured: ${error}`);
-        }
-      }
-    );
+    this.postService.createPost(post).subscribe((response: any) => {
+      post.id = response.id;
+      this.posts.unshift(post);
+    });
   }
 
   updatePost(post: any) {
-    this.postService.updatePost(post).subscribe(
-      (response) => {
-        console.log(response);
-      },
-      (error) => {
-        console.log(`Unexpected Error has occured: ${error}`);
-      }
-    );
+    this.postService.updatePost(post).subscribe((response) => {
+      console.log(response);
+    });
   }
 
   deletePost(post: any) {
-    this.postService.deletePost(post.id).subscribe(
-      (response) => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      },
-      (error: Response) => {
-        if (error.status === 404) {
-          console.log('This post has already been deleted.');
-        } else {
-          console.log(`Unexpected Error has occured: ${error}`);
-        }
-      }
-    );
+    this.postService.deletePost(post.id).subscribe((response) => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    });
   }
 }
