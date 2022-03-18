a19e457a2a78a25b13f2b0e8ba4f640f77888d6e
EliezerAmbion <eliezerambion@gmail.com>
2022-03-18 16:21:15 +0800

    map operator

--- PROJECTS and EXERCISES/authors-exercise-project/src/app/services/data.service.ts
+++ PROJECTS and EXERCISES/authors-exercise-project/src/app/services/data.service.ts
@@ -1,24 +1,28 @@
+import { map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 export class DataService {
   constructor(private url: string, private http: HttpClient) {}
 
-  get() {
-    return this.http.get(this.url);
+  getAll() {
+    return this.http.get(this.url).pipe(map((response: any) => response));
   }
 
   create(resource: any) {
-    return this.http.post(this.url, JSON.stringify(resource));
+    return this.http
+      .post(this.url, JSON.stringify(resource))
+      .pipe(map((response: any) => response));
   }
 
   update(resource: any) {
-    return this.http.patch(
-      this.url + '/' + resource.id,
-      JSON.stringify({ isRead: true })
-    );
+    return this.http
+      .patch(this.url + '/' + resource.id, JSON.stringify({ isRead: true }))
+      .pipe(map((response: any) => response));
   }
 
   delete(id: any) {
-    return this.http.delete(this.url + '/' + id);
+    return this.http
+      .delete(this.url + '/' + id)
+      .pipe(map((response: any) => response));
   }
 }
