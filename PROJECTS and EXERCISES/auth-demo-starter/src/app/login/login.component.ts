3e3e0a933d36b39525ccf290e5aa14b1918da3e2
EliezerAmbion <eliezerambion@gmail.com>
2022-04-10 16:02:16 +0800

    redirecting users after logging in

--- PROJECTS and EXERCISES/auth-demo-starter/src/app/login/login.component.ts
+++ PROJECTS and EXERCISES/auth-demo-starter/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { AuthService } from "./../services/auth.service";
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "login",
@@ -10,12 +10,27 @@ import { Router } from "@angular/router";
 export class LoginComponent {
   invalidLogin: boolean = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  // NOTE: In order to access route parameters, we need to inject the activated route service.
+  constructor(
+    private readonly _router: Router,
+    private readonly _authService: AuthService,
+    private readonly _route: ActivatedRoute
+  ) {}
 
   signIn(credentials) {
-    this.authService.login(credentials).subscribe((result) => {
-      if (result) this.router.navigate(["/"]);
-      else this.invalidLogin = true;
+    this._authService.login(credentials).subscribe((result) => {
+      if (result) {
+        // NOTE: Now here wecan safely use the snapshot, because on this login page,
+        // we don't have a link or a button that takes the user to the same page.
+        // In other words, we don't need to subscribe to the query paramMap observable.
+        let returnUrl = this._route.snapshot.queryParamMap.get("returnUrl");
+
+        // navigate to returnUrl if you have a returnUrl in your route parameters,
+        // otherwise, navigate to the homepage.
+        this._router.navigate([returnUrl || "/"]);
+      } else {
+        this.invalidLogin = true;
+      }
     });
   }
 }
--- PROJECTS and EXERCISES/auth-demo-starter/src/app/services/auth-guard.service.ts
+++ PROJECTS and EXERCISES/auth-demo-starter/src/app/services/auth-guard.service.ts
@@ -10,24 +10,22 @@ import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  // NOTE: in order to check to see if the user is looged in or not, we need to inject our authService
-  // NOTE: in order for us to navigate the user to the login page, we need to inject the router service.
   constructor(
     private readonly _authService: AuthService,
     private readonly _router: Router
   ) {}
 
-  // NOTE: canActivate - in this method we need to check to see if the user is logged in or not.
-  // If they're logged in we're going to return true.
-  // Otherwise we should navigate the user to the login page, and return false.
-  canActivate() {
+  // NOTE: canActivate taked two parameters, one is route, and the other is state.
+  // State parameter is of type RouterStateSnapshot
+  // REMEMBER: with the state paramter, we can get access to the URL that the user wanted to access.
+  canActivate(route, state: RouterStateSnapshot) {
     if (this._authService.isLoggedIn()) return true;
 
     // REMEMBER: in the parameters array of navigate, the first element is the path,
-    // second element is optional.
-
-    // NOTE: you can navigate a user to anywhere but the login page would be the best.
-    this._router.navigate(["/login"]);
+    // second element is an object with one property.
+    this._router.navigate(["/login"], {
+      queryParams: { returnUrl: state.url },
+    });
     return false;
   }
 }
